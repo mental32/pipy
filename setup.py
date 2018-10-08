067275ae373f761718f797f4b5a168a3ce29e9c0
mental <m3nta1@yahoo.com>
2018-10-09 00:49:44 +0100

    bugfix setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ import re
 from distutils.core import setup
 
 def get_version():
-  with open('pipy/__main__.py') as inf:
+    with open('pipy/__main__.py') as inf:
       match = re.search(r"((\d\.){2,5}\d)", inf.read(), re.MULTILINE)
 
       if match is None:
           raise RuntimeError('Version could not be found.')
-
-return match.groups()[0]
+    return match.groups()[0]
 
 setup(
     name='pipy',
